7aa1ca253b63e7967729caa851f4963eeb66e851
duliangliang <lldu@zhishu.com>
2020-04-28 21:32:04 +0800

    20200428 添加zuul前置过滤器

--- zs_getway/src/main/java/zsgetway/configure/filter/TokenFilter.java
+++ zs_getway/src/main/java/zsgetway/configure/filter/TokenFilter.java
@@ -1,15 +1,24 @@
-package zsgetway.configure;
+package zsgetway.configure.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class TokenFilter extends ZuulFilter {
+
+    /*
+    * pre: 这种过滤器在请求被路由之前调用。可利用这种过滤器实现身份验证、在集群中选择请求的微服务，记录调试信息等。
+      routing: 这种过滤器将请求路由到微服务。这种过滤器用于构建发送给微服务的请求，并使用apache httpclient或netflix ribbon请求微服务。
+      post: 这种过滤器在路由到微服务以后执行。这种过滤器可用来为响应添加标准的http header、收集统计信息和指标、将响应从微服务发送给客户端等。
+      error: 在其他阶段发送错误时执行该过滤器。
+    *
+    * */
 
     public boolean shouldFilter() {   //判断过滤器是否生效
         // TODO Auto-generated method stub
@@ -37,8 +46,8 @@ public class TokenFilter extends ZuulFilter {
 
     @Override
     public String filterType() {     //过滤器的类型
-        // TODO Auto-generated method stub
-        return "pre";   //表示在请求之前执行
+
+        return FilterConstants.PRE_TYPE;   //表示在请求之前执行
     }
 
     @Override
