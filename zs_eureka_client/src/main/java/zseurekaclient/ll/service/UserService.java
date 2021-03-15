78a7f85210b1445ec676ebfe1061be13b7ee4512
duliangliang <lldu@zhishu.com>
2021-03-15 11:11:11 +0800

    20210315 添加eureka

---
+++ zs_eureka_client/src/main/java/zseurekaclient/ll/service/UserService.java
@@ -0,0 +1,9 @@
+package zseurekaclient.ll.service;
+
+import zseurekaclient.ll.bean.ConsultContent;
+
+import java.util.List;
+
+public interface UserService {
+    List<ConsultContent> queryContents();
+}
---
+++ zs_eureka_client/src/main/java/zseurekaclient/ll/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package zseurekaclient.ll.service.impl;
+
+import zseurekaclient.ll.bean.ConsultContent;
+import zseurekaclient.ll.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl  implements UserService {
+    @Override
+    public List<ConsultContent> queryContents() {
+        return null;
+    }
+}
